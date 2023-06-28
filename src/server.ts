5608115584b19fed1ecba5ddf9c685b02e21b212
erykscarabellodias <erykscarabello97@gmail.com>
2023-06-27 21:53:18 -0300

    chore: changing code style to airbnb

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 
 app.listen(3000, () => {
-	const message = 'App rodando na porta 3000...';
+  const message = "App rodando na porta 3000...";
 
-	console.log(message);
+  console.log(message);
 });
